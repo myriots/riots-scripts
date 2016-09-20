70cefb5b43684f99e5e49ad2fc3f2a740b376b76
Riots <info@riots.fi>
2016-09-20 12:30:45 +0300

    Added missing Copyright information

--- Riots_Air.py
+++ Riots_Air.py
@@ -1,3 +1,18 @@
+'''
+ * This file is part of Riots.
+ * Copyright © 2016 Riots Global OY; <copyright@myriots.com>
+ *
+ * Riots is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+ *
+ * Riots is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Riots.
+ * If not, see <http://www.gnu.org/licenses/>.
+'''
+
+#!/usr/bin/python
 import time
 import serial
 import os
